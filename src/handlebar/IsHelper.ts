8102e46aca29b8bdaff0fa2e2e59024483677164
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-09-19 11:02:08 +0000

    fix: Enhance `is` helper

--- src/handlebar/IsHelper.ts
+++ src/handlebar/IsHelper.ts
@@ -6,23 +6,68 @@ import logger from "../logger";
 export class IsHelper {
     /**
      * Registers is helper
-     * - Helps in comparision of two values
-     * - Expects two values, i.e. value1 and value2
+     * - Extends builtin if helper functionalities
      * @returns {void}
      */
     register = () => {
-        Handlebars.registerHelper("is", (context) => {
-            logger.debug(JSON.stringify((context.hash)));
-            if (typeof context.hash.value1 === "undefined" || typeof context.hash.value2 === "undefined") {
-                logger.error("Two values are required for comparision.");
-                return "N/A";
-            } else {
-                if (context.hash.value1 === context.hash.value2) {
-                    return context.fn(this);
-                } else {
-                    return context.inverse(this);
-                }
+        Handlebars.registerHelper("is", (...args) => {
+            let left: any;
+            let right: any;
+            let operator: any;
+            let context: any;
+            switch (args.length) {
+                case 2:
+                    left = args[0];
+                    context = args[1];
+                    if (left) {
+                        return context.fn(this);
+                    } else {
+                        return context.inverse(this);
+                    }
+                case 3:
+                    left = args[0];
+                    right = args[1];
+                    context = args[2];
+                    if (left === right) {
+                        return context.fn(this);
+                    } else {
+                        return context.inverse(this);
+                    }
+                case 4:
+                    left = args[0]
+                    operator = args[1]
+                    right = args[2]
+                    context = args[3]
+                    if (this.evaluateOperator(operator, left, right)) {
+                        return context.fn(this);
+                    } else {
+                        return context.inverse(this);
+                    }
+                default:
+                    logger.error("Incorrect number of arguments")
+                    break;
             }
+            return true;
         });
     };
+    private evaluateOperator = (operator: string, left: any, right: any | any[]) => {
+        switch (operator) {
+            case "not":
+                return left != right;
+            case ">":
+                return left > right;
+            case "<":
+                return left < right;
+            case ">=":
+                return left >= right;
+            case "<=":
+                return left <= right;
+            case "===":
+                return left === right;
+            case "!==":
+                return left !== right;
+            default:
+                break;
+        }
+    }
 }
