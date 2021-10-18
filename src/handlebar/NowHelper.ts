077dd7ba9288d54f1672cde6f87368e65dbf04b1
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-10-18 08:30:38 +0000

    fix: erroneous Now Helper fix

--- src/handlebar/NowHelper.ts
+++ src/handlebar/NowHelper.ts
@@ -1,32 +1,39 @@
 import logger from "../logger";
+import moment from "moment";
 /**
- * Defines and registers custom handlebar helper - num_between
+ * Defines and registers custom handlebar helper - now
  */
-export class NumBetweenHelper {
+export class NowHelper {
   private Handlebars: any;
   constructor(Handlebars: any) {
     this.Handlebars = Handlebars
   }
   /**
-   * Registers num_between helper
-   * - If lower or upper value is not passed, return 0
-   * - If lower value is greater than upper value, log error and return 0
+   * Registers now helper
+   * - If now helper is called without a format, set a default format as YYYY-MM-DD hh:mm:ss else use the format provided
+   * - Set default offset to be used if offset is not specified. Default offset is 0s i.e. no offset
+   * - If offset is defined the value will be stored in context.hash.offset, eg X days.
+   * - Split value by a space, first element will be the amount of offset i.e. X, second element will be unit of offset, i.e. days
+   * - Return a value with specified format and added offset
    * @returns {void}
    */
   register = () => {
-    this.Handlebars.registerHelper("num_between", (context: any) => {
-      if (typeof context.hash.lower === "undefined" || typeof context.hash.upper === "undefined") {
-        logger.error("lower or upper value not specified.");
-        return 0;
-      } else {
-        const lower = parseInt(context.hash.lower);
-        const upper = parseInt(context.hash.upper);
-        if (lower > upper) {
-          logger.error("lower value cannot be greater than upper value.");
-          return 0;
-        }
-        const num = Math.floor(Math.random() * (upper - lower + 1) + lower);
-        return num;
+    this.Handlebars.registerHelper("now", (context: any) => {
+      const format = typeof context.hash.format === "undefined" ? "YYYY-MM-DD hh:mm:ss" : context.hash.format;
+      let offsetUnit: moment.unitOfTime.DurationConstructor = "s";
+      let offsetAmount: number = 0;
+      if (typeof context.hash.offset !== "undefined") {
+        let offset = context.hash.offset.split(" ");
+        offsetAmount = <number>offset[0];
+        offsetUnit = <moment.unitOfTime.DurationConstructor>offset[1];
+      }
+      switch (format) {
+        case "epoch":
+          return moment().add(offsetAmount, offsetUnit).format("x");
+        case "unix":
+          return moment().add(offsetAmount, offsetUnit).format("X");
+        default:
+          return moment().add(offsetAmount, offsetUnit).format(format);
       }
     });
   };
