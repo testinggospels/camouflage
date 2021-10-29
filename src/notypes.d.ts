3ca3c3a0c1c169de583a8bd797b0e37d09253d34
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-10-29 12:36:30 +0530

    fix: remove @ts-ignore

---
+++ src/notypes.d.ts
@@ -0,0 +1,5 @@
+// Instead of using @ts-ignore, this file is used to define modules for which types are not installed/available/required
+declare module 'apicache';
+declare module '@opuscapita/filemanager-server';
+declare module 'swagger-stats';
+declare module 'cors';
