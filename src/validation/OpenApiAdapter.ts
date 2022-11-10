426b5e2cfb9f9677a6df183238ceba95b097a4bb
Richard Ruiter <richard.ruiter@gmail.com>
2022-11-10 09:28:56 +0100

    fix: validating parameterized paths on the openapi is now supported

--- src/validation/OpenApiAdapter.ts
+++ src/validation/OpenApiAdapter.ts
@@ -9,35 +9,72 @@ import logger from "../logger";
 import { ValidationAdapter, ValidationResult } from "./ValidationAdapter";
 import type { HttpParserResponse } from "../parser/HttpParser";
 
-const expressifyPath = (path: string) => {
-  /* eslint-disable */
-  const params = path.match(/(\{(?:\{.*\}|[^\{])*\})/g);
-  /* eslint-enable */
-  if (params?.length > 0) {
-    for (let x = 0; x < params.length; x++) {
-      const param = params[x];
-      path = path.replace(param, param.replace("{", ":").replace("}", ""));
-    }
-  }
-  return path;
-};
-
 export default class OpenApiAdapter extends ValidationAdapter {
   private document: OpenAPI.Document;
 
   private findRoute(req: Request) {
     const { paths } = this.document;
-    const route = Object.entries(paths).find(([p]) => {
-      const expressPath = expressifyPath(p);
+    const route = Object.entries(paths).find(([p, v]) => {
+      const expressPath = this.expressifyPath(p);
+      v.url = p; // assign the url
       const url = req.url.split("?")[0];
       return (
         expressPath === req.route.path ||
         expressPath === url ||
         expressPath.slice(0, -1) === url ||
-        expressPath === url.slice(0, -1)
+        expressPath === url.slice(0, -1) ||
+        this.urlMatchesParsedRoute(expressPath, url)
       );
     });
     return route && route[1];
+  }
+
+  private urlMatchesParsedRoute(route: string, url: string) {
+    const routeParts = this.getParts(route);
+    const urlParts = this.getParts(url);
+    const parsedRoute = routeParts.map((part: string, x: number) => {
+      if (part.startsWith(":")) {
+        return urlParts[x];
+      }
+      return part;
+    });
+    const parsedUrl = `/${parsedRoute.join("/")}`;
+    if (parsedUrl === url) {
+      return true;
+    }
+    return false;
+  }
+
+  private extractPathParamsFromRequest(req: Request) {
+    const currentRoute = this.findRoute(req);
+    const routeParts = this.getParts(this.expressifyPath(currentRoute.url));
+    const urlParts = this.getParts(req.url.split("?")[0]);
+    const pathParams: { [k: string]: string } = {};
+    routeParts.forEach((part: string, x: number) => {
+      if (part.startsWith(":")) {
+        pathParams[part.slice(1)] = urlParts[x];
+      }
+    });
+    return pathParams;
+  }
+
+  private expressifyPath(path: string) {
+    /* eslint-disable */
+    const params = path.match(/(\{(?:\{.*\}|[^\{])*\})/g);
+    /* eslint-enable */
+    if (params?.length > 0) {
+      for (let x = 0; x < params.length; x++) {
+        const param = params[x];
+        path = path.replace(param, param.replace("{", ":").replace("}", ""));
+      }
+    }
+    return path;
+  }
+
+  private getParts(url: string) {
+    const parts = url.split("/");
+    parts.shift();
+    return parts;
   }
 
   async load(): Promise<void> {
@@ -67,6 +104,9 @@ export default class OpenApiAdapter extends ValidationAdapter {
       const requestValidator = new OpenAPIRequestValidator(
         currentRoute[method]
       );
+
+      req.params = this.extractPathParamsFromRequest(req);
+
       const result = requestValidator.validateRequest(req);
 
       if (result?.errors.length > 0) {
